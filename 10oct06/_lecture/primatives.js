0160fbef8fdb101eb8552bacb1c6dc3fb2f0af6a
Eddie Gurnee <pegurnee@gmail.com>
2015-10-06 12:56:35 -0400

    add basic today

---
+++ 10oct06/_lecture/primatives.js
@@ -0,0 +1,43 @@
+/*
+ * As of ES6, there are 6 primative types:
+ *
+ * String
+ * Number
+ * Boolean
+ * Null
+ * Undefined
+ * Symbol
+ */
+
+// String
+{
+  var textHere = 'Some input text';
+  var otherText = "More text here";
+  var haxxor = `${textHere} interpolated!`;
+
+  console.log(textHere);
+  console.log(otherText);
+  console.log(haxxor);
+}
+
+// Number
+{
+  var someNum = 3;
+  var moreNum = 3.14;
+  var yetMore = -Infinity; // or +Infinity, NaN
+
+  console.log(someNum);
+  console.log(moreNum);
+  console.log(yetMore);
+}
+
+// Boolean
+{
+  var goForward = true;
+  var goBack = !goForward;
+  var goWhereEver = (goBack || true) && (goForward && !false);
+
+  console.log(goForward);
+  console.log(goBack);
+  console.log(goWhereEver);
+}
