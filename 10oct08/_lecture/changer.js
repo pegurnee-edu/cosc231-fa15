521c1b329d937e76112bdb8b7fac2296ee1b382f
teacher <teacher@cs-514-teacher.local>
2015-10-08 17:16:44 -0400

    lecture from today

--- 10oct08/_lecture/changer.js
+++ 10oct08/_lecture/changer.js
@@ -1,7 +1,32 @@
-function colorBlue() {
-  var items = document.getElementsByTagName('p');
+// function colorBlue() {
+//   var items = document.getElementsByTagName('p');
+// //   items[20] = 'data';
+// 
+// // 	for (var i = 0; i < items.length; i++ {
+// // 		items[i].style.color = 'blue';
+// // 	}
+// 
+//   for (var item of items) {
+//     item.style.color = 'blue';
+//   }
+// }
 
+function colorRed() {
+	var items = document.querySelectorAll('p, footer');
+	
   for (var item of items) {
-    item.style.color = 'blue';
+    item.style.color = 'red';
+  }
+  
+  var greenNodes = document.querySelectorAll('.green');
+   
+   for (var e of greenNodes) {
+    e.style.color = 'green';
   }
 }
+
+
+
+
+
+
