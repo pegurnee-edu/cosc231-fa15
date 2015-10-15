29ad0b220f7dc276a3a3a3c2a8eb7249304db926
Eddie <egurnee@emich.edu>
2015-10-15 15:56:32 -0400

    more today

--- 10oct15/_lecture/a_array.js
+++ 10oct15/_lecture/a_array.js
@@ -1,8 +1,30 @@
-var ar1 = [],
-	ar = [5, 7, 'items'];
-	
-ar1[0] = 5;
+var ar = [5, 7, 'items'];
+console.log(ar);
 
-console.log(ar[2]); //items
+ar[3] = 42;
+console.log(ar);
+
+ar = 4;
 
 console.log(ar);
+ar = [];
+ar[2] = 'val 1';
+ar[5] = 'val 3';
+console.log(ar);
+
+ar['hello'] = 'something';
+console.log(ar);
+
+console.log(ar['hello']);
+index = 'hello';
+
+console.log(ar[index]);
+
+for (x of ar) {
+  //console.log(x);
+}
+
+for (x in ar) {
+  console.log('key ' + x + '\nvalue: ' +ar[x]);
+}
+
